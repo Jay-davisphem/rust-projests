9f14a745d97910d9fa174edadb29d99375db2b3d
David Oluwafemi <davidoluwafemi178@gmail.com>
2024-01-07 14:35:27 +0100

    add carol twelve days lyrics generator and matching

--- simple_exercises/carol-lyrics/src/main.rs
+++ simple_exercises/carol-lyrics/src/main.rs
@@ -1,3 +1,39 @@
+const TWELVE_DAYS: [(&str, &str); 12] = [
+    ("first", "A partridge in a pear tree"),
+    ("second", "Two turtle doves"),
+    ("third", "Three French hens"),
+    ("fourth", "Four calling birds"),
+    ("fifth", "Five golden rings"),
+    ("sixth", "Six geese a-laying"),
+    ("seventh", "Seven swans a-swimming"),
+    ("eighth", "Eight maids a-milking"),
+    ("ninth", "Nine ladies dancing"),
+    ("tenth", "Ten lords a-leaping"),
+    ("eleventh", "Eleven pipers piping"),
+    ("twelfth", "Twelve drummers drumming"),
+];
+
 fn main() {
-    println!("Hello, world!");
+    for i in 0..TWELVE_DAYS.len() {
+        let (day, _) = TWELVE_DAYS[i];
+
+        display_generic_lyrics(day);
+
+        let mut j = 0;
+        while j <= i {
+            let stringj = TWELVE_DAYS[i - j].1;
+            print!("{stringj}");
+            if i - j == 0 {
+                println!(".");
+            } else {
+                println!(",");
+            }
+            j += 1;
+        }
+        println!();
+    }
+}
+
+fn display_generic_lyrics(day: &str) {
+    println!("On the {day} day of Christmas,\nmy true love gave to me")
 }
