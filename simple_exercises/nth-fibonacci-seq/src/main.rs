aa70d805f388da0b0b3ff85a77879cd2deb93254
David Oluwafemi <davidoluwafemi178@gmail.com>
2024-01-07 13:59:25 +0100

    add fibonacci

--- simple_exercises/nth-fibonacci-seq/src/main.rs
+++ simple_exercises/nth-fibonacci-seq/src/main.rs
@@ -1,3 +1,26 @@
 fn main() {
-    println!("Hello, world!");
+    println!("{}", r_nth_fibonacci(20));
+    println!("{}", nth_fibonacci(20))
+}
+
+fn r_nth_fibonacci(n: i32) -> i64 {
+    if n < 3 {
+        1
+    } else {
+        r_nth_fibonacci(n - 1) + r_nth_fibonacci(n - 2)
+    }
+}
+
+fn nth_fibonacci(n: i32) -> i64 {
+    let mut first = 0;
+    let mut second = 1;
+
+    let mut i = 0;
+    while i < n - 1 {
+        let tmp = second;
+        second += first;
+        first = tmp;
+        i += 1;
+    }
+    second
 }
